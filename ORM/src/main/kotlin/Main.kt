2c8aad66fbd7cfffe445ac34604c1f99e5cd087d
Marina Kalugina <21mk07@mail.ru>
2018-06-21 03:26:15 +0300

    add deleteAll/deleteByPK in ORM

--- ORM/src/main/kotlin/Main.kt
+++ ORM/src/main/kotlin/Main.kt
@@ -3,11 +3,10 @@ import java.lang.IllegalArgumentException
 import java.sql.DriverManager
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import kotlin.reflect.full.cast
+import java.util.concurrent.ConcurrentLinkedDeque
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.declaredMembers
 import kotlin.reflect.jvm.javaType
-import kotlin.reflect.jvm.jvmErasure
 
 annotation class Table(val name: String)
 
@@ -43,7 +42,6 @@ class Repository(url : String, username : String, password : String){
 
     inline fun <reified T> createTable(){
         val tableName = T::class.annotations.find { it is Table }?.let { (it as Table).name } ?: throw IllegalArgumentException("Аргументом должна быть таблица")
-        T::class.declaredMembers.map { listOf(it.name, it.returnType.isMarkedNullable) }
         val str = T::class.declaredMembers.map { it.name + " " + convert(it.returnType.javaType.typeName) + if (it.annotations.any { it is PrimaryKey }) " primary key" else "" + if (it.returnType.isMarkedNullable) "" else " not null"}
         connection.createStatement().executeUpdate("create table " + tableName + "( " + str.joinToString(",") + ")")
     }
@@ -64,21 +62,50 @@ class Repository(url : String, username : String, password : String){
         values.forEachIndexed { i, s -> statement.setObject(i + 1, s) }
         statement.executeUpdate()
     }
+
+    inline fun <reified T> selectAll() : ConcurrentLinkedDeque<T>?{
+        val tableName = T::class.annotations.find { it is Table }?.let { (it as Table).name } ?: throw IllegalArgumentException("Аргументом должна быть таблица")
+        val statement = connection.prepareStatement("select * from " + tableName)
+        val res = statement.executeQuery()
+        val resArray : ConcurrentLinkedDeque<T> = ConcurrentLinkedDeque()
+        while (res.next()){
+            var constr = T::class.constructors.elementAt(0)
+            resArray.add(constr.call(*constr.parameters.map {
+                when (it.type.javaType.typeName){
+                    "java.lang.String" -> res.getString(it.name)
+                    "java.time.LocalDateTime" -> {
+                        LocalDateTime.parse(res.getString(it.name), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))
+                    }
+                    "int" -> res.getInt(it.name)
+                    else -> res.getObject(it.name)
+                }
+            }.toTypedArray()))
+        }
+        return resArray
+    }
+
+    inline fun <reified T> deleteAll(){
+        val tableName = T::class.annotations.find { it is Table }?.let { (it as Table).name } ?: throw IllegalArgumentException("Аргументом должна быть таблица")
+        val statement = connection.prepareStatement("delete from " + tableName)
+        statement.executeUpdate()
+    }
+
+    inline fun <reified T> deleteByPK(id : Int){
+        val tableName = T::class.annotations.find { it is Table }?.let { (it as Table).name } ?: throw IllegalArgumentException("Аргументом должна быть таблица")
+        val field =  T::class.declaredMembers.find { it.annotations.any { it is PrimaryKey }  }?.name ?: throw IllegalArgumentException("Отсутствует первичный ключ")
+        val statement = connection.prepareStatement("delete from " + tableName + " where " + field + "=" + id)
+        statement.executeUpdate()
+    }
+
+
 }
 
 fun main(args: Array<String>) {
     val repository = Repository("jdbc:postgresql://localhost:5432/db", "marina", "1234")
 //    repository.createTable<Personage>()
-//    repository.insert(Personage("qwerty'); DROP TABLE Personages --", LocalDateTime.now(), 1))
-    print(repository.findByPK<Personage>(1))
-    FastClasspathScanner("").matchClassesWithAnnotation(Table::class.java, { cls ->
-        cls.name
-        cls.annotatedInterfaces
-        cls.annotations.find { it is Table }?.let { (it as Table).name }
-        cls.kotlin.declaredMembers.map { listOf(it.name, it.returnType.isMarkedNullable) }
-        cls.kotlin.declaredMembers.map { listOf(it.name, it.returnType.javaType.typeName) }
-    }).scan()
-
+//    repository.insert(Personage("qwerty", LocalDateTime.now(), 1))
+//    repository.insert(Personage("12345", LocalDateTime.now(), 2))
+    println(repository.findByPK<Personage>(1))
+    repository.deleteByPK<Personage>(1)
+    println(repository.selectAll<Personage>())
 }
-
-//cls.kotlin.primaryConstructor.parameters.map { it.name }
